9980b2fec9d35ba2827b9ea0c9cf7b4f57f7230d
Victoria Trzcianowski <vtrzcianowski@gmail.com>
2017-07-17 17:12:09 -0400

    mobile first

--- app/index.js
+++ app/index.js
@@ -1,10 +1,95 @@
 require('./main.scss');
 
 let indexer = require('file-loader?name=../dist/index.html!./index.html');
+let cycle2 = require('./cycle2.js');
 
 $(document).ready(function(){
   $('.stretch').each(function () {
       $(this).stretch_text();
+  });
+
+  $( '.mySlideshows' ).cycle();
+
+  // document.getElementById('pro1').addEventListener("click", function(){
+  //   alert("click");
+  // })
+  $('#desc1click').click(function() {
+    $('#desc2').fadeOut('slow', function() {
+      $('#desc3').fadeOut('slow', function() {
+        $('#desc4').fadeOut('slow', function() {
+          $('#desc5').fadeOut('slow', function() {
+            $('#desc6').fadeOut('slow', function() {
+              $('#desc1').fadeIn('slow', function() {});
+            });
+          });
+        });
+      });
+    });
+  });
+  $('#desc2click').click(function() {
+    $('#desc1').fadeOut('slow', function() {
+      $('#desc3').fadeOut('slow', function() {
+        $('#desc4').fadeOut('slow', function() {
+          $('#desc5').fadeOut('slow', function() {
+            $('#desc6').fadeOut('slow', function() {
+              $('#desc2').fadeIn('slow', function() {});
+            });
+          });
+        });
+      });
+    });
+  });
+  $('#desc3click').click(function() {
+    $('#desc1').fadeOut('slow', function() {
+      $('#desc2').fadeOut('slow', function() {
+        $('#desc4').fadeOut('slow', function() {
+          $('#desc5').fadeOut('slow', function() {
+            $('#desc6').fadeOut('slow', function() {
+              $('#desc3').fadeIn('slow', function() {});
+            });
+          });
+        });
+      });
+    });
+  });
+  $('#desc4click').click(function() {
+    $('#desc1').fadeOut('slow', function() {
+      $('#desc2').fadeOut('slow', function() {
+        $('#desc3').fadeOut('slow', function() {
+          $('#desc5').fadeOut('slow', function() {
+            $('#desc6').fadeOut('slow', function() {
+              $('#desc4').fadeIn('slow', function() {});
+            });
+          });
+        });
+      });
+    });
+  });
+  $('#desc5click').click(function() {
+    $('#desc1').fadeOut('slow', function() {
+      $('#desc2').fadeOut('slow', function() {
+        $('#desc3').fadeOut('slow', function() {
+          $('#desc4').fadeOut('slow', function() {
+            $('#desc6').fadeOut('slow', function() {
+              $('#desc5').fadeIn('slow', function() {});
+            });
+          });
+        });
+      });
+    });
+  });
+  $('#desc6click').click(function() {
+    $('#desc1').fadeOut('slow', function() {
+      $('#desc2').fadeOut('slow', function() {
+        $('#desc3').fadeOut('slow', function() {
+          $('#desc4').fadeOut('slow', function() {
+            $('#desc5').fadeOut('slow', function() {
+              $('#desc6').fadeIn('slow', function() {});
+            });
+          });
+        });
+      });
+    });
   });
   let currentYear = new Date().getFullYear();     // Get the four digit year (yyyy)
   document.getElementById('copyright').innerHTML = "copyright " + currentYear;
@@ -56,3 +141,60 @@ $.fn.stretch_text = function () {
         elmt.addClass('justify');
     }
 };
+
+// $('#pro1').click(function() {
+//   alert('clicked');
+//   // $('#project1').hide('slow', function() {
+//
+//     // Animation complete.
+//   // });
+// });
+//
+// let dot1 = document.getElementById("pro1").addEventListener("click", function(){
+//     document.getElementById("project1").style.display = "block";
+//     document.getElementById("project2").style.display = "none";
+//     document.getElementById("project3").style.display = "none";
+//     document.getElementById("project4").style.display = "none";
+//     document.getElementById("project5").style.display = "none";
+//     document.getElementById("project6").style.display = "none";
+// });
+// let dot2 = document.getElementById("pro2").addEventListener("click", function(){
+//     document.getElementById("project2").style.display = "block";
+//     document.getElementById("project1").style.display = "none";
+//     document.getElementById("project3").style.display = "none";
+//     document.getElementById("project4").style.display = "none";
+//     document.getElementById("project5").style.display = "none";
+//     document.getElementById("project6").style.display = "none";
+// });
+// let dot3 = document.getElementById("pro3").addEventListener("click", function(){
+//     document.getElementById("project3").style.display = "block";
+//     document.getElementById("project2").style.display = "none";
+//     document.getElementById("project1").style.display = "none";
+//     document.getElementById("project4").style.display = "none";
+//     document.getElementById("project5").style.display = "none";
+//     document.getElementById("project6").style.display = "none";
+// });
+// let dot4 = document.getElementById("pro4").addEventListener("click", function(){
+//     document.getElementById("project4").style.display = "block";
+//     document.getElementById("project2").style.display = "none";
+//     document.getElementById("project3").style.display = "none";
+//     document.getElementById("project1").style.display = "none";
+//     document.getElementById("project5").style.display = "none";
+//     document.getElementById("project6").style.display = "none";
+// });
+// let dot5 = document.getElementById("pro5").addEventListener("click", function(){
+//     document.getElementById("project5").style.display = "block";
+//     document.getElementById("project2").style.display = "none";
+//     document.getElementById("project3").style.display = "none";
+//     document.getElementById("project4").style.display = "none";
+//     document.getElementById("project1").style.display = "none";
+//     document.getElementById("project6").style.display = "none";
+// });
+// let dot6 = document.getElementById("pro6").addEventListener("click", function(){
+//     document.getElementById("project6").style.display = "block";
+//     document.getElementById("project2").style.display = "none";
+//     document.getElementById("project3").style.display = "none";
+//     document.getElementById("project4").style.display = "none";
+//     document.getElementById("project5").style.display = "none";
+//     document.getElementById("project1").style.display = "none";
+// });
